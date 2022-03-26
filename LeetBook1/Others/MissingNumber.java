b3864a51bc8a4ebaaac38f109a649581d7d06210
huxi <1286993582@qq.com>
2022-03-26 14:50:19 +0800

    修改1

--- LeetBook1/Others/MissingNumber.java
+++ LeetBook1/Others/MissingNumber.java
@@ -1,17 +1,9 @@
 package Others;
 
+import java.util.Arrays;
+
 public class MissingNumber {
     public int missingNumber(int[] nums) {
-        // Arrays.sort(nums);
-        // int res = 0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(res == nums[i]){
-        //         res++;
-        //     }else{
-        //         break;
-        //     }
-        // }
-        // return res;
         boolean[] flag = new boolean[nums.length + 1];
         int i = 0;
         for (; i < nums.length; i++) {
@@ -20,5 +12,18 @@ public class MissingNumber {
         i = 0;
         while (flag[i]) i++;
         return i;
+    }
+
+    public int missingNumber2(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for (int num : nums) {
+            if (res == num) {
+                res++;
+            } else {
+                break;
+            }
+        }
+        return res;
     }
 }
